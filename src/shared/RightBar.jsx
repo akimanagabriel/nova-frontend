ecfce4161cdd4237912ff36720adbf5e9580e79e
akimanagabriel <gabsonbill@yahoo.com>
2022-10-28 14:20:47 +0200

    add right menulists

--- src/shared/RightBar.jsx
+++ src/shared/RightBar.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { services } from './menuLinks'
 
 function RightBar() {
   return (
     <div className='w-full md:w-3/12 bg-gray-100 px-3 justify-self-end hidden lg:block'>
       <h1 className='text-lg md:text-2xl font-bold text-sky-900 my-2'>Services</h1>
-      <ul className='space-y-3'>
-        <li>Training</li>
-        <li>Training</li>
-        <li>Training</li>
-        <li>Training</li>
-        <li>Training</li>
-        <li>Training</li>
-        <li>Training</li>
+      <ul className='space-y-2'>
+        {
+          services.map((service) => (
+            <li>
+              <NavLink to={service.url} className='hover:underline'> {service.title} </NavLink>
+            </li>
+          ))
+        }
       </ul>
     </div>
   )
---
+++ src/shared/menuLinks.js
@@ -0,0 +1,44 @@
+export const services = [
+    {
+        id: 1,
+        title: 'Training',
+        url: '/training',
+        description: 'Describe how your business can help solve the visitors problems.'
+    },
+    {
+        id: 2,
+        title: 'Software Development',
+        url: '/software-development',
+        description: 'Describe how your business can help solve the visitors problems.'
+    },
+    {
+        id: 3,
+        title: 'Business Advertisement',
+        url: '/business',
+        description: 'Describe how your business can help solve the visitors problems.'
+    },
+    {
+        id: 4,
+        title: 'Online Services',
+        url: '/online-services',
+        description: 'Describe how your business can help solve the visitors problems.'
+    },
+    {
+        id: 5,
+        title: 'Computer Maintenance',
+        url: '/computer-maintenance',
+        description: 'Describe how your business can help solve the visitors problems.'
+    },
+    {
+        id: 6,
+        title: 'Networking',
+        url: '/networking',
+        description: 'Describe how your business can help solve the visitors problems.'
+    },
+    {
+        id: 7,
+        title: 'CCTV Cameras',
+        url: '/cctv',
+        description: 'Describe how your business can help solve the visitors problems.'
+    },
+]
