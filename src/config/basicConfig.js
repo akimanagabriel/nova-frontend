1085ff9fe1b46b5acb6cab97b334cd9889e9e90a
akimanagabriel <gabsonbill@yahoo.com>
2022-12-01 12:30:02 +0200

    changes

--- src/config/basicConfig.js
+++ src/config/basicConfig.js
@@ -1,5 +1,5 @@
 const api = {
-  offlineUrl: "http://localhost:5000",
+  offlineUrl: "https://backend-nova.herokuapp.com",
   testingUrl: "https://backend-nova.herokuapp.com",
   productionUrl: "",
   googleClientId: "3299070349-n8aqm9af5if97a01qf0k2l270v5gp67p.apps.googleusercontent.com"
